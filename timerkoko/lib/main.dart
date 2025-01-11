67ceba1a30f2e18deb1ff3aef57d1ea9c5aece4b
Dukeazeta <azetaduke@gmail.com>
2025-01-11 08:41:16 +0100

    modified:   timerkoko/lib/main.dart

--- timerkoko/lib/main.dart
+++ timerkoko/lib/main.dart
@@ -45,7 +45,8 @@ class TimerScreen extends StatefulWidget {
   State<TimerScreen> createState() => _TimerScreenState();
 }
 
-class _TimerScreenState extends State<TimerScreen> with SingleTickerProviderStateMixin {
+class _TimerScreenState extends State<TimerScreen>
+    with SingleTickerProviderStateMixin {
   late AnimationController _spinAnimationController;
 
   @override
@@ -68,7 +69,8 @@ class _TimerScreenState extends State<TimerScreen> with SingleTickerProviderStat
     return Scaffold(
       backgroundColor: Colors.grey[50],
       body: GetBuilder<TimerController>(
-        init: TimerController(spinAnimationController: _spinAnimationController),
+        init:
+            TimerController(spinAnimationController: _spinAnimationController),
         builder: (controller) {
           return SafeArea(
             child: Center(
@@ -113,11 +115,15 @@ class _TimerScreenState extends State<TimerScreen> with SingleTickerProviderStat
                           painter: CustomTimerPainter(
                             progress: controller.progress,
                             isRunning: controller.isRunning,
+                            isSettingTime: controller.isSettingTime,
                             spinAnimation: _spinAnimationController,
+                            setupRotation: controller.setupRotation,
                           ),
                           size: const Size(300, 300),
                         ),
                         GestureDetector(
+                          onPanStart: controller.onPanStart,
+                          onPanEnd: controller.onPanEnd,
                           onPanUpdate: controller.onPanUpdate,
                           child: Container(
                             height: 300,
@@ -129,7 +135,9 @@ class _TimerScreenState extends State<TimerScreen> with SingleTickerProviderStat
                         ),
                         IconButton(
                           icon: Icon(
-                            controller.isRunning ? Icons.pause : Icons.play_arrow,
+                            controller.isRunning
+                                ? Icons.pause
+                                : Icons.play_arrow,
                             size: 32,
                             color: Colors.white,
                           ),
@@ -156,12 +164,16 @@ class _TimerScreenState extends State<TimerScreen> with SingleTickerProviderStat
 class CustomTimerPainter extends CustomPainter {
   final double progress;
   final bool isRunning;
+  final bool isSettingTime;
   final Animation<double> spinAnimation;
+  final double setupRotation;
 
   CustomTimerPainter({
     required this.progress,
     required this.isRunning,
+    required this.isSettingTime,
     required this.spinAnimation,
+    required this.setupRotation,
   }) : super(repaint: spinAnimation);
 
   @override
@@ -169,25 +181,28 @@ class CustomTimerPainter extends CustomPainter {
     final center = Offset(size.width / 2, size.height / 2);
     final radius = size.width / 2;
 
-    // Draw progress arc
-    final progressPaint = Paint()
-      ..color = isRunning ? Colors.red : Colors.red[400]!
-      ..style = PaintingStyle.fill;
+    final rotationAngle =
+        isRunning ? -spinAnimation.value * 2 * math.pi : setupRotation;
 
-    final rotationAngle = isRunning ? spinAnimation.value * 2 * math.pi : 0.0;
-    
     canvas.save();
     canvas.translate(center.dx, center.dy);
     canvas.rotate(rotationAngle);
     canvas.translate(-center.dx, -center.dy);
 
-    canvas.drawArc(
-      Rect.fromCircle(center: center, radius: radius - 15),
-      -math.pi / 2,
-      2 * math.pi * progress,
-      true,
-      progressPaint,
-    );
+    // Only draw progress arc if setting time or running
+    if (isSettingTime || isRunning) {
+      final progressPaint = Paint()
+        ..color = isRunning ? Colors.red : Colors.red[400]!
+        ..style = PaintingStyle.fill;
+
+      canvas.drawArc(
+        Rect.fromCircle(center: center, radius: radius - 15),
+        -math.pi / 2,
+        2 * math.pi * progress,
+        true,
+        progressPaint,
+      );
+    }
 
     // Draw numbers
     for (var i = 0; i < 12; i++) {
@@ -197,7 +212,10 @@ class CustomTimerPainter extends CustomPainter {
         text: TextSpan(
           text: number,
           style: TextStyle(
-            color: _isOverProgress(angle, progress) ? Colors.white : Colors.black87,
+            color:
+                (isSettingTime || isRunning) && _isOverProgress(angle, progress)
+                    ? Colors.white
+                    : Colors.black87,
             fontSize: 16,
             fontWeight: FontWeight.w600,
           ),
@@ -205,11 +223,13 @@ class CustomTimerPainter extends CustomPainter {
         textDirection: TextDirection.ltr,
       );
       textPainter.layout();
-      
+
       final numberRadius = radius - 35;
-      final x = center.dx + numberRadius * math.cos(angle) - textPainter.width / 2;
-      final y = center.dy + numberRadius * math.sin(angle) - textPainter.height / 2;
-      
+      final x =
+          center.dx + numberRadius * math.cos(angle) - textPainter.width / 2;
+      final y =
+          center.dy + numberRadius * math.sin(angle) - textPainter.height / 2;
+
       textPainter.paint(canvas, Offset(x, y));
     }
 
@@ -229,9 +249,11 @@ class CustomTimerPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(CustomTimerPainter oldDelegate) {
-    return oldDelegate.progress != progress || 
-           oldDelegate.isRunning != isRunning ||
-           oldDelegate.spinAnimation.value != spinAnimation.value;
+    return oldDelegate.progress != progress ||
+        oldDelegate.isRunning != isRunning ||
+        oldDelegate.isSettingTime != isSettingTime ||
+        oldDelegate.spinAnimation.value != spinAnimation.value ||
+        oldDelegate.setupRotation != setupRotation;
   }
 }
 
@@ -241,9 +263,25 @@ class TimerController extends GetxController {
   double progress = 0.0;
   int remainingSeconds = 0;
   late AnimationController spinAnimationController;
+  double setupRotation = 0.0;
+  bool isSettingTime = false;
 
   TimerController({required AnimationController spinAnimationController}) {
     this.spinAnimationController = spinAnimationController;
+  }
+
+  void onPanStart(DragStartDetails details) {
+    if (!isRunning) {
+      isSettingTime = true;
+      update();
+    }
+  }
+
+  void onPanEnd(DragEndDetails details) {
+    if (!isRunning) {
+      isSettingTime = false;
+      update();
+    }
   }
 
   void onPanUpdate(DragUpdateDetails details) {
@@ -251,14 +289,31 @@ class TimerController extends GetxController {
 
     final box = Get.context!.findRenderObject() as RenderBox;
     final center = Offset(box.size.width / 2, box.size.height / 2);
-    final position = details.localPosition;
-    final angle = (math.atan2(position.dy - center.dy, position.dx - center.dx) +
-            math.pi / 2) %
-        (2 * math.pi);
-    
-    selectedMinutes = ((angle / (2 * math.pi)) * 60).round();
-    progress = angle / (2 * math.pi);
+    final touchPoint = details.localPosition;
+
+    // Calculate angle from center to touch point
+    final angle = math.atan2(
+      touchPoint.dy - center.dy,
+      touchPoint.dx - center.dx,
+    );
+
+    // Convert angle to progress (0 to 1), reversed for counter-clockwise
+    double newProgress = 1.0 - ((angle + math.pi) / (2 * math.pi));
+    // Adjust for starting from top (+ 0.25 for counter-clockwise)
+    newProgress = (newProgress + 0.25) % 1.0;
+
+    // Update rotation for setup animation (negative for counter-clockwise)
+    setupRotation = -newProgress * 2 * math.pi;
+
+    // Calculate minutes (0 to 60)
+    selectedMinutes = (newProgress * 60).round();
+    if (selectedMinutes == 0 && newProgress > 0.99) {
+      selectedMinutes = 60;
+    }
+
     remainingSeconds = selectedMinutes * 60;
+    progress = newProgress;
+
     update();
   }
 
